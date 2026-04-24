b153fd8e97794e6a7489450eaccde8b1bf4cfade
JZPro-ux <jsilvalopes84@gmail.com>
2026-04-24 15:30:45 +0100

    Delete files

--- README.md
+++
@@ -1,2 +0,0 @@
-# Project_SGR
-Sistema de Gestão de Resíduos da Extração e Transformação da Pedra Natural
--- backend/app/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from .utilizador import Utilizador
-from .localizacao import Localizacao
-from .materia_prima import MateriaPrima
-from .tipo_residuo import TipoResiduo
-from .residuo import Residuo
-from .destino_residuo import DestinoResiduo
-from .configuracao import Configuracao
